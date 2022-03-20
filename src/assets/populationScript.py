ce85a0589cafd6878d17765b3ce111098a1bef03
alecron <alecronster@gmail.com>
2022-03-20 19:30:56 +0100

    Alt.routes considered and small code improvements

--- src/assets/populationScript.py
+++ src/assets/populationScript.py
@@ -1,4 +1,4 @@
-from errno import EIDRM, ENOANO
+from tokenize import String
 import pandas as pd 
 import os
 import csv
@@ -8,21 +8,46 @@ import xmltodict
 from elasticsearch import Elasticsearch
 from jsonpath_ng import jsonpath, parse
 
+ALT_PATH = '"gmd:characterSet"."gmd:parentIdentifier".'
 
 # JsonPath routes
 JsonPathExpr_id = parse('$."gmd:MD_Metadata"."gmd:fileIdentifier"."gco:CharacterString"')
 JsonPathExpr_language = parse('$."gmd:MD_Metadata"."gmd:language"."gmd:LanguageCode"')
-JsonPathExpr_date = parse('$."gmd:MD_Metadata"."gmd:dateStamp"."gco:Date"')
 
-JsonPathExpr_scope = parse('$."gmd:MD_Metadata"."gmd:hierarchyLevelName"."gco:CharacterString"')
-JsonPathExpr_standard = parse('$."gmd:MD_Metadata"."gmd:metadataStandardName"."gco:CharacterString"')
-JsonPathExpr_standardVersion = parse('$."gmd:MD_Metadata"."gmd:metadataStandardVersion"."gco:CharacterString"')
+JsonPathExpr = {
+'date' : parse('$."gmd:MD_Metadata"."gmd:dateStamp"."gco:Date"') ,
+'scope' : parse('$."gmd:MD_Metadata"."gmd:hierarchyLevelName"."gco:CharacterString"') ,
+'standard' : parse('$."gmd:MD_Metadata"."gmd:metadataStandardName"."gco:CharacterString"') ,
+'standardVersion' : parse('$."gmd:MD_Metadata"."gmd:metadataStandardVersion"."gco:CharacterString"') ,
+'contactPoint_name' : parse('$."gmd:MD_Metadata"."gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:organisationName"."gco:CharacterString"') ,
+'contactPoint_mail' : parse('$."gmd:MD_Metadata"."gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:contactInfo"."gmd:CI_Contact"."gmd:address"."gmd:CI_Address"."gmd:electronicMailAddress"."gco:CharacterString"') ,
+'contactPoint_onlineSource' : parse('$."gmd:MD_Metadata"."gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:contactInfo"."gmd:CI_Contact"."gmd:onlineResource"."gmd:CI_OnlineResource"."gmd:linkage"."gmd:URL"') ,
+'accessUrl' : parse('$."gmd:MD_Metadata"."gmd:distributionInfo"."gmd:MD_Distribution"."gmd:transferOptions"."gmd:MD_DigitalTransferOptions"."gmd:onLine"."gmd:CI_OnlineResource"."gmd:linkage"."gmd:URL"') ,
+}
 
-JsonPathExpr_contactPoint_name = parse('$."gmd:MD_Metadata"."gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:organisationName"."gco:CharacterString"')
-JsonPathExpr_contactPoint_mail = parse('$."gmd:MD_Metadata"."gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:contactInfo"."gmd:CI_Contact"."gmd:address"."gmd:CI_Address"."gmd:electronicMailAddress"."gco:CharacterString"')
-JsonPathExpr_contactPoint_onlineSource = parse('$."gmd:MD_Metadata"."gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:contactInfo"."gmd:CI_Contact"."gmd:onlineResource"."gmd:CI_OnlineResource"."gmd:linkage"."gmd:URL"')
+Alt_JsonPathExpr = {
+'date' : parse('$."gmd:MD_Metadata".{0}"gmd:dateStamp"."gco:Date"'.format(ALT_PATH)) ,
+'scope' : parse('$."gmd:MD_Metadata".{0}"gmd:hierarchyLevelName"."gco:CharacterString"'.format(ALT_PATH)) ,
+'standard' : parse('$."gmd:MD_Metadata".{0}"gmd:metadataStandardName"."gco:CharacterString"'.format(ALT_PATH)) ,
+'standardVersion' : parse('$."gmd:MD_Metadata".{0}"gmd:metadataStandardVersion"."gco:CharacterString"'.format(ALT_PATH)) ,
+'contactPoint_name' : parse('$."gmd:MD_Metadata".{0}"gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:organisationName"."gco:CharacterString"'.format(ALT_PATH)) ,
+'contactPoint_mail' : parse('$."gmd:MD_Metadata".{0}"gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:contactInfo"."gmd:CI_Contact"."gmd:address"."gmd:CI_Address"."gmd:electronicMailAddress"."gco:CharacterString"'.format(ALT_PATH)) ,
+'contactPoint_onlineSource' : parse('$."gmd:MD_Metadata".{0}"gmd:identificationInfo"."srv:SV_ServiceIdentification"."gmd:pointOfContact"."gmd:CI_ResponsibleParty"."gmd:contactInfo"."gmd:CI_Contact"."gmd:onlineResource"."gmd:CI_OnlineResource"."gmd:linkage"."gmd:URL"'.format(ALT_PATH)) ,
+'accessUrl' : parse('$."gmd:MD_Metadata".{0}"gmd:distributionInfo"."gmd:MD_Distribution"."gmd:transferOptions"."gmd:MD_DigitalTransferOptions"."gmd:onLine"."gmd:CI_OnlineResource"."gmd:linkage"."gmd:URL"'.format(ALT_PATH)) ,
+}
 
-JsonPathExpr_accessUrl = parse('$."gmd:MD_Metadata"."gmd:distributionInfo"."gmd:MD_Distribution"."gmd:transferOptions"."gmd:MD_DigitalTransferOptions"."gmd:onLine"."gmd:CI_OnlineResource"."gmd:linkage"."gmd:URL"')
+
+def getPathValue(field, jsonObj):
+    foundInExpr = JsonPathExpr[field].find(jsonObj)
+    if(len(foundInExpr) <= 0):
+        foundInAlt = Alt_JsonPathExpr[field].find(jsonObj)
+        if(len(foundInAlt) <= 0):
+            return ""
+        else:
+            return foundInAlt[0].value    
+    else:
+        return foundInExpr[0].value
+
 
 # Find altrentative routes. There are some files that doesn't have the same routes so maybe something like "if(value) is null -> try alt routes"
 # It can still get the available functionalities. They are a list of funcitons found as "containsOperations"
@@ -41,16 +66,18 @@ for index, row in metadataFile.iterrows():
     
     value_ID = JsonPathExpr_id.find(jsonObj)[0].value
     value_language = JsonPathExpr_language.find(jsonObj)[0].value
-    value_date = JsonPathExpr_date.find(jsonObj)[0].value
-    value_scope = JsonPathExpr_scope.find(jsonObj)[0].value
-    value_standard = JsonPathExpr_standard.find(jsonObj)[0].value
-    value_standardVersion = JsonPathExpr_standardVersion.find(jsonObj)[0].value
-    value_contactPoint_name = JsonPathExpr_contactPoint_name.find(jsonObj)[0].value
-    value_contactPoint_mail = JsonPathExpr_contactPoint_mail.find(jsonObj)[0].value
-    value_contactPoint_onlineSource = JsonPathExpr_contactPoint_onlineSource.find(jsonObj)[0].value
-    value_accessUrl = JsonPathExpr_accessUrl.find(jsonObj)[0].value
     
-    print("################################################")
+    value_date = getPathValue("date", jsonObj)
+    value_scope = getPathValue("scope", jsonObj)
+    value_standard = getPathValue("standard", jsonObj)
+    value_standardVersion = getPathValue("standardVersion", jsonObj)
+    value_contactPoint_name = getPathValue("contactPoint_name", jsonObj)
+    value_contactPoint_mail = getPathValue("contactPoint_mail", jsonObj)
+    value_contactPoint_onlineSource = getPathValue("contactPoint_onlineSource", jsonObj)
+    value_accessUrl = getPathValue("accessUrl", jsonObj)
+
+    
+    print("################################################################################################")
     print(value_ID)
     print(value_language)
     print(value_date)
@@ -61,6 +88,7 @@ for index, row in metadataFile.iterrows():
     print(value_contactPoint_mail)
     print(value_contactPoint_onlineSource)
     print(value_accessUrl)
+    print("################################################################################################")
 
 
     # Load data to ElasticSearch
