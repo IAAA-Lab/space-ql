267aa17b4710df4408b81933e83b381ea93099fc
alecron <alecronster@gmail.com>
2022-06-02 00:11:06 +0200

    Not working - Facet functionality

--- src/jsMain/kotlin/frontend/app/App.kt
+++ src/jsMain/kotlin/frontend/app/App.kt
@@ -38,8 +38,48 @@ val app = FC<Props> {
     }
 
     useEffect(facetsList) {
-        val resultAux : MutableList<MetadataRecord>  = ArrayList(resultList)
+        // TODO - treat facets on the backend or send the whole content
+        // idea: Mandar en el search() unos campos language, resource type y related docs
+        // y en base a ellos hacer el filtrado y rehacer la busqueda.
+        // deberia ser sencillo si cada campo es un arrayList de Strings que
+        // contenga los campos que deben mirarse en el filter
+        // ej: language = [Spanish, English, Other] y si esta vacio que no filtre en base
+        // a eso
+        if(facetsList.isNotEmpty()){
+            var resultAux : MutableList<MetadataRecord>  = ArrayList(resultList)
+            // Language - Spanish, English, Other/Unknown
+            val langs = facetsList.find { el -> el.name == "Language" }
 
+            val spa = langs?.values?.find{ el -> el.field == "Spanish"}?.checked
+            val eng = langs?.values?.find{ el -> el.field == "English"}?.checked
+            val otherLang = langs?.values?.find{ el -> el.field == "Other/Unknown"}?.checked
+
+            console.log(spa, eng, otherLang)
+
+            var langFilter = ArrayList<String>()
+            val spaFilter = arrayOf("Spanish", "Español", "spa" )
+            val engFilter = arrayOf("eng", "English")
+
+            if(spa == true){
+                langFilter.addAll(spaFilter)
+            }
+            if(eng == true){
+                langFilter.addAll(engFilter)
+            }
+
+            console.log(resultAux.size)
+            if(otherLang == true){
+                resultAux = resultAux.filter { el -> el.details?.language in langFilter || (el.details?.language !in spaFilter && el.details?.language !in engFilter )  } as MutableList<MetadataRecord>
+            } else if(spa == true || eng == true){
+                resultAux = resultAux.filter { el -> el.details?.language in langFilter } as MutableList<MetadataRecord>
+            }
+            console.log(resultAux.size)
+
+            resultList = resultAux
+        }
+
+        // resource type - Service, Dataset, Other
+        // related documents - 0 1 2 3 +3
     }
 
     fun getResultsProp(term: String, offset: Int, order: String) {
