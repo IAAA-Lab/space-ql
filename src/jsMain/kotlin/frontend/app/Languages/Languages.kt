9356cf203f42a56bedb46e9fb21980b79c12a428
alecron <alecronster@gmail.com>
2022-06-20 11:46:29 +0200

    Internacionalization - Button and title (#25)

---
+++ src/jsMain/kotlin/frontend/app/Languages/Languages.kt
@@ -0,0 +1,8 @@
+package frontend.app.Languages
+
+val langMap = mapOf(
+    "title" to mapOf(
+        "Esp" to "Buscador de metadatos basado en GraphQL",
+        "Eng" to "GraphQL-based metadata browser"
+    )
+)
